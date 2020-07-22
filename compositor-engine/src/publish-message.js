d393b819c30a20ef798b99939ede439485587dbd
jonaskay <jonaskay@iki.fi>
2020-07-22 20:55:37 +0300

    Convert data to a buffer

--- compositor-engine/src/publish-message.js
+++ compositor-engine/src/publish-message.js
@@ -2,6 +2,7 @@ const { PubSub } = require("@google-cloud/pubsub")
 
 module.exports = (siteId, topicName) => {
   const data = JSON.stringify({ id: siteId, status: "deployed" })
+  const dataBuffer = Buffer.from(data)
 
-  return new PubSub().topic(topicName).publish(data)
+  return new PubSub().topic(topicName).publish(dataBuffer)
 }
