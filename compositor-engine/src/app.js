8f121dc4674a7ce8ab65e5a02835076043f8d072
jonaskay <jonaskay@iki.fi>
2020-07-22 10:48:17 +0300

    Extract functions

--- compositor-engine/src/app.js
+++ compositor-engine/src/app.js
@@ -4,22 +4,26 @@ const createConfig = require("./create-config")
 const runProcess = require("./run-process")
 const deleteInstance = require("./delete-instance")
 
+const runBuildProcess = () => {
+  return runProcess(
+    "yarn",
+    ["workspace", "compositor-template", "build", "--prefix-paths"],
+    path.resolve(__dirname)
+  )
+}
+
+const runCopyProcess = () => {
+  return runProcess("bin/copy", [], path.resolve(__dirname))
+}
+
 module.exports = () => {
   const storageBucket = process.env.GOOGLE_STORAGE_BUCKET
   const siteId = process.env.HOSTNAME
   const templateDir = path.resolve(__dirname, "..", "compositor-template")
 
   createConfig(templateDir, storageBucket, siteId)
-    .then(() => {
-      return runProcess(
-        "yarn",
-        ["workspace", "compositor-template", "build", "--prefix-paths"],
-        path.resolve(__dirname)
-      )
-    })
-    .then(() => {
-      return runProcess("bin/copy", [], path.resolve(__dirname))
-    })
+    .then(() => runBuildProcess())
+    .then(() => runCopyProcess())
     .then(() => {
       return deleteInstance(
         process.env.CLOUD_PROJECT_ID,
